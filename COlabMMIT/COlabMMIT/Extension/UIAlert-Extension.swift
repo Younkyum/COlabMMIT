f0b22e09b936a2c0ae47c51243bcf63f530e40a6
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-06-01 21:39:05 +0900

    UI - 1

---
+++ COlabMMIT/COlabMMIT/Extension/UIAlert-Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIAlert-Extension.swift
+//  COlabMMIT
+//
+//  Created by Jin younkyum on 2022/06/01.
+//
+
+import UIKit
+
+extension UIViewController {
+   func showInfoAlert(with message: String) {
+      showErrorAlert(with: message, title: "Info")
+   }
+   
+   func showErrorAlert(with message: String, title: String = "Error") {
+      DispatchQueue.main.async {
+         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+         
+         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+         alert.addAction(okAction)
+         
+         self.present(alert, animated: true, completion: nil)
+      }
+   }
+}
---
+++ COlabMMIT/COlabMMIT/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  COlabMMIT
+//
+//  Created by Jin younkyum on 2022/06/01.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
